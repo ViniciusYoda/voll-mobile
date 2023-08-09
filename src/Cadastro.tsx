f71cd791393a9682d9899e0296ec47f7d6160a22
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-09 07:55:54 -0300

    tela de cadastro

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -1,63 +1,56 @@
-import { VStack, Image, Text, Box, Link } from 'native-base'
+import { VStack, Image, Text, Box, Link, Checkbox, ScrollView } from 'native-base'
+import { useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
 import { Botao } from './componentes/Botao';
 import { EntradaTexto } from './componentes/EntradaTexto';
 import { Titulo } from './componentes/Titulo';
-import { useState } from 'react'
+import { secoes } from './utils/CadastroEntradaTexto';
 
-export default function Cadastro() {
-  const [numSecao, setNumSecao] = useState()
-  const secoes = [
-    {
-      id: 1,
-      titulo: 'Insiraalguns dados básicos',
-      entradaTexto: [
-        {
-          id: 1,
-          label: 'Nome',
-          placeholder: 'Digite seu nome completo'
-        },
-        {
-          id: 2,
-          label: 'Email',
-          placeholder: 'Digite seu email'
-        },
-      ]
-    },
-    {
-      id: 2,
-      titulo: 'Agora, mais alguns dados sobre você:',
-      entradaTexto: [
-        {
-          id: 1,
-          label: 'Cep',
-          placeholder: 'Digite seu cep'
-        }
-      ]
+export default function Login() {
+  const [numSecao, setNumSecao] = useState(0);
+  
+
+  function avancarSecao(){
+    if(numSecao < secoes.length - 1){
+      setNumSecao(numSecao+1)
     }
-  ]
+  }
 
-  function avancarSecao() {
-    setNumSecao(numSecao+1)
+  function voltarSecao(){
+    if(numSecao > 0){
+      setNumSecao(numSecao - 1)
+    }
   }
 
   return (
-    <VStack flex={1} alignItems="center" justifyContent="center" p={5}>
-      <Image source={Logo} alt="Logo Voll" />
+    <ScrollView flex={1} p={5}>
+      <Image source={Logo} alt="Logo Voll" alignSelf="center" />
 
       <Titulo>
         {secoes[numSecao].titulo}
       </Titulo>
       <Box>
         {
-          secoes[numSecao].entradaTexto.map(entrada => {
+          secoes[numSecao]?.entradaTexto?.map(entrada => {
             return <EntradaTexto label={entrada.label} placeholder={entrada.placeholder} key={entrada.id} />
           })
         }
       </Box>
-      <Botao onPress={() => avancaSecao()}>Avanlar</Botao>
-
-    </VStack>
+      <Box>
+        <Text color="blue.800" fontWeight="bold" fontSize="md" mt="2" mb={2}>
+          Selecione o plano:
+        </Text>
+        {
+          secoes[numSecao].checkbox.map(checkbox => {
+            return <Checkbox key={checkbox.id} value={checkbox.value}>
+              {checkbox.value}
+            </Checkbox>
+          })
+        }
+      </Box>
+      {numSecao > 0 && <Botao onPress={() => voltarSecao()} bgColor="gray.400">Voltar</Botao>}
+      <Botao onPress={() => avancarSecao()} mt={4} mb={20}>Avançar</Botao>
+    </ScrollView>
   );
 }
