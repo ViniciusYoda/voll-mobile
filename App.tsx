495eb6a9c8714d0eac19cfb13909cdafbeaa6df7
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-10 09:54:25 -0300

    conectado com api

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import Rotas from './src/Rotas';
 
 
 export default function App() {
+
   return (
     <NativeBaseProvider theme={TEMAS}>
       <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
