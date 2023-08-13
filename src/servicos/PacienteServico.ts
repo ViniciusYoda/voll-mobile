58aa47e576e474c76afcdadf401570e80de13e3e
Vinícius Yoda <vyoda4604@gmail.com>
2023-08-13 11:28:23 -0300

    perfil

--- src/servicos/PacienteServico.ts
+++ src/servicos/PacienteServico.ts
@@ -12,3 +12,14 @@ export async function cadastrarPaciente(paciente: Paciente) {
         return null
     }
 }
+
+export async function pegarDadosPaciente(id: string) {
+    try {
+        const resultado = await api.get(`/paciente/${id}`)
+        return resultado.data
+    }
+    catch (error) {
+        console.log(error)
+        return null
+    }
+}
